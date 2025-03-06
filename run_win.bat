6fed8f7812cf4f6be4856452789f1e94ac633556
juanhdzma <juanhdzma@pm.me>
2025-03-05 21:10:16 -0500

    Change venv to python and update bat

--- run_win.bat
+++ run_win.bat
@@ -1,4 +1,22 @@
 @echo off
-cd /d "%~dp0"
-call app\venv\Scripts\activate
-python app\main.py
+setlocal
+
+:: Obtener la ruta del directorio donde está el script .bat
+set "BASE_PATH=%~dp0"
+
+:: Construir el comando
+set "COMMAND=%BASE_PATH%app\python-3\python.exe app\main.py"
+
+:: Mostrar el comando en la terminal
+echo %COMMAND%
+
+:: Esperar entrada del usuario antes de ejecutarlo
+pause
+
+:: Ejecutar el comando
+%COMMAND%
+
+endlocal
+
+
+:: C:\Users\juanhdzma\Downloads\App\python-3\python.exe app\main.py
