7b510969c66fd78512936078404130889ab24c73
juanhdzma <juanhdzma@pm.me>
2025-03-11 13:39:19 -0500

    Fix request

--- App/src/models/api_consumer.py
+++ App/src/models/api_consumer.py
@@ -1,21 +1,25 @@
 from src.util.error import CustomError
 import subprocess
-import shlex
 
 
-USE_PROXY = False
+USE_PROXY = True
 PROXY = "http://172.19.152.108:8000"
 
 
 def fetch_data(url_data):
     try:
-        curl_base = "curl -s -k -L"
+        curl_base = "curl -k -L"
         if USE_PROXY:
-            curl_base += f" -x {shlex.quote(PROXY)}"
+            curl_base += f" -x {PROXY}"
 
-        cmd_content = f"{curl_base} {shlex.quote(url_data)}"
+        cmd_content = f"{curl_base} {url_data}"
         result = subprocess.run(
-            cmd_content, shell=True, capture_output=True, text=True, check=True
+            cmd_content,
+            shell=True,
+            capture_output=True,
+            text=True,
+            check=True,
+            encoding="utf-8",
         )
 
         return result.stdout
